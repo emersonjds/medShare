39e79ce9cd7c797020875d2cb75915ead1e0b46b
Emerson Silva <emerson_jds@live.com>
2023-10-05 16:47:11 -0300

    add new items

--- src/components/TableOne.tsx
+++ src/components/TableOne.tsx
@@ -4,6 +4,35 @@ import BrandThree from '../images/brand/brand-03.svg';
 import BrandFour from '../images/brand/brand-04.svg';
 import BrandFive from '../images/brand/brand-05.svg';
 
+import Chair from '../images/items/cadeira.png';
+import Tipoia from '../images/items/tipoia.png';
+import Andador from '../images/items/andador.png';
+import Muleta from '../images/items/muleta.png';
+import Cama from '../images/items/cama.png';
+import Bola from '../images/items/bola.jpeg';
+
+import * as styles from './styles/TableOne';
+
+const items = [
+  {
+    id: 1,
+    name: 'Cadeira de Rodas',
+    image: Chair,
+    quantity: 3500,
+    price: 5768,
+    rented: 590,
+    roi: 4.8,
+  },
+  {
+    id: 2,
+    name: 'Tipoia',
+    image: Tipoia,
+    quantity: 3500,
+    price: 5768,
+    rented: 590,
+  }
+]
+
 const TableOne = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
@@ -41,11 +70,20 @@ const TableOne = () => {
         </div>
 
         <div className="grid grid-cols-3 border-b border-stroke dark:border-strokedark sm:grid-cols-5">
+
+
+
+
           <div className="flex items-center gap-3 p-2.5 xl:p-5">
             <div className="flex-shrink-0">
-              <img src={BrandOne} alt="Brand" />
+              <img
+                style={{
+                  height: '60px',
+                  width: '60px',
+                }}
+                src={Chair} alt="Brand" />
             </div>
-            <p className="hidden text-black dark:text-white sm:block">Google</p>
+            <p className="hidden text-black dark:text-white sm:block">Andador</p>
           </div>
 
           <div className="flex items-center justify-center p-2.5 xl:p-5">
@@ -63,15 +101,19 @@ const TableOne = () => {
           <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
             <p className="text-meta-5">4.8%</p>
           </div>
+
         </div>
 
         <div className="grid grid-cols-3 border-b border-stroke dark:border-strokedark sm:grid-cols-5">
           <div className="flex items-center gap-3 p-2.5 xl:p-5">
             <div className="flex-shrink-0">
-              <img src={BrandTwo} alt="Brand" />
+              <img src={Andador} alt="Brand" style={{
+                height: '60px',
+                width: '60px',
+              }} />
             </div>
             <p className="hidden text-black dark:text-white sm:block">
-              Twitter
+              Andador
             </p>
           </div>
 
@@ -95,9 +137,14 @@ const TableOne = () => {
         <div className="grid grid-cols-3 border-b border-stroke dark:border-strokedark sm:grid-cols-5">
           <div className="flex items-center gap-3 p-2.5 xl:p-5">
             <div className="flex-shrink-0">
-              <img src={BrandThree} alt="Brand" />
+              <img src={Muleta} alt="Brand"
+                style={{
+                  height: '60px',
+                  width: '60px',
+                }}
+              />
             </div>
-            <p className="hidden text-black dark:text-white sm:block">Github</p>
+            <p className="hidden text-black dark:text-white sm:block">Muleta</p>
           </div>
 
           <div className="flex items-center justify-center p-2.5 xl:p-5">
@@ -120,9 +167,14 @@ const TableOne = () => {
         <div className="grid grid-cols-3 border-b border-stroke dark:border-strokedark sm:grid-cols-5">
           <div className="flex items-center gap-3 p-2.5 xl:p-5">
             <div className="flex-shrink-0">
-              <img src={BrandFour} alt="Brand" />
+              <img src={Bola} alt="Brand"
+                style={{
+                  height: '60px',
+                  width: '60px',
+                }}
+              />
             </div>
-            <p className="hidden text-black dark:text-white sm:block">Vimeo</p>
+            <p className="hidden text-black dark:text-white sm:block">Bola Fisioterapia</p>
           </div>
 
           <div className="flex items-center justify-center p-2.5 xl:p-5">
@@ -145,10 +197,15 @@ const TableOne = () => {
         <div className="grid grid-cols-3 sm:grid-cols-5">
           <div className="flex items-center gap-3 p-2.5 xl:p-5">
             <div className="flex-shrink-0">
-              <img src={BrandFive} alt="Brand" />
+              <img src={Tipoia} alt="Brand"
+                style={{
+                  height: '60px',
+                  width: '60px',
+                }}
+              />
             </div>
             <p className="hidden text-black dark:text-white sm:block">
-              Facebook
+              Tipoia
             </p>
           </div>
 
@@ -168,7 +225,10 @@ const TableOne = () => {
             <p className="text-meta-5">1.9%</p>
           </div>
         </div>
+
+
       </div>
+
     </div>
   );
 };
